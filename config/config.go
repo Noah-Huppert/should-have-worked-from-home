e44ca8d7966edb0fb412fd56a6c4c1302d5d2dfb
Noah Huppert <contact@noahh.io>
2018-05-05 15:46:57 -0400

    added gapi options to config

--- config/config.go
+++ config/config.go
@@ -4,12 +4,32 @@ import "os"
 import "fmt"
 
 // EnvKeySlackToken is the environment variable key the Slack token will be
-// provided under
+// provided by
 const EnvKeySlackToken string = "SLACK_TOKEN"
+
+// EnvKeyGAPIKeyPath is the environment variable key the path to the GAPI
+// key file will be provided by
+const EnvKeyGAPIKeyPath string = "GAPI_KEY_PATH"
+
+// EnvKeySpreadsheetStore is the environment variable key which the id of the
+// spreadsheet information will be stored in will be provided by
+const EnvKeySpreadsheetStore string = "SPREADSHEET_STORE"
+
+// DefaultGAPIKeyPath is default location of the GAPI key file if none is
+// provided
+const DefaultGAPIKeyPath string = "gapi_key.json"
 
 type Config struct {
 	// SlackToken is the Slack API token
 	SlackToken string
+
+	// GAPIKeyPath is the path to the Google API key file. It defaults to
+	// DefaultGAPIKeyPath if not specified by an environment variable
+	GAPIKeyPath string
+
+	// SpreadsheetStore is the id of the Google spreadsheet which "I
+	// should have worked from home today" messages will be recorded in
+	SpreadsheetStore string
 }
 
 // New constructs a new Config from environment variables. An error is returned
@@ -22,9 +42,24 @@ func New() (*Config, error) {
 			"provided", EnvKeySlackToken)
 	}
 
+	// Get GAPIKeyPath if set
+	gapiKeyPath, ok := os.LookupEnv(EnvKeyGAPIKeyPath)
+	if !ok {
+		gapiKeyPath = DefaultGAPIKeyPath
+	}
+
+	// Get SpreadsheetStore
+	spreadsheetStore, ok := os.LookupEnv(EnvKeySpreadsheetStore)
+	if !ok {
+		return nil, fmt.Errorf("environment variable \"%s\" must be "+
+			"provided", EnvKeySpreadsheetStore)
+	}
+
 	// Make instance
 	c := Config{
-		SlackToken: slackToken,
+		SlackToken:       slackToken,
+		GAPIKeyPath:      gapiKeyPath,
+		SpreadsheetStore: spreadsheetStore,
 	}
 
 	return &c, nil
